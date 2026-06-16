c946346a0ff1ad232242f2158319263a3551cf57
Ubuntu <ubuntu@ip-10-0-1-176.ap-south-1.compute.internal>
2026-06-16 16:50:20 +0000

    feat: migrate Terraform state to S3 backend

--- one-click-infra/provider.tf
+++ one-click-infra/provider.tf
@@ -1,9 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "oneclick-tfstate-155734788051"
+    key            = "oneclick/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "oneclick-tfstate-lock"
+    encrypt        = true
   }
 }
 
