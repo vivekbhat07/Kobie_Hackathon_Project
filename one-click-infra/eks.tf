018e8c75822731e1e6550f093345fca29f0de836
vaishjp <vaishjp@gmail.com>
2026-06-18 17:24:05 +0530

    feat: install EBS CSI driver addon via IRSA, fixes PVC binding

--- one-click-infra/eks.tf
+++ one-click-infra/eks.tf
@@ -15,3 +15,36 @@ resource "aws_eks_cluster" "main" {
     aws_iam_role_policy_attachment.cluster_policy
   ]
 }
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_irsa.arn
+
+  depends_on = [aws_eks_node_group.main]
+}
+
+resource "aws_iam_role" "ebs_csi_irsa" {
+  name = "oneclick-ebs-csi-irsa"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
+  role       = aws_iam_role.ebs_csi_irsa.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
