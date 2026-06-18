773b27b164c6c871d0d7bcdba9701e5826fe3f25
vaishjp <vaishjp@gmail.com>
2026-06-18 16:48:10 +0530

    fix: consolidate RDS SG rules into single inline ingress block, eliminate apply oscillation

--- one-click-infra/security_groups.tf
+++ one-click-infra/security_groups.tf
@@ -46,7 +46,6 @@ resource "aws_security_group" "rds_sg" {
   name        = "rds-security-group"
   description = "Managed by Terraform"
   vpc_id      = aws_vpc.main.id
-
   ingress {
     description     = "Postgres from EKS nodes only"
     from_port       = 5432
@@ -54,24 +53,20 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.eks_node_sg.id]
   }
-
+  ingress {
+    description     = "Postgres from EKS cluster auto-assigned node SG"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_eks_cluster.main.vpc_config[0].cluster_security_group_id]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "rds-security-group"
   }
-}
-resource "aws_security_group_rule" "rds_from_eks_cluster_sg" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.rds_sg.id
-  source_security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-  description               = "Postgres from EKS cluster auto-assigned node SG"
 }
