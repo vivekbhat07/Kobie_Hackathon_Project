5eab6551157d4807f637626c92272fb7398373ed
vaishjp <vaishjp@gmail.com>
2026-06-20 15:39:06 +0530

    fix: grant ReadOnlyAccess to jenkins IRSA for drift detection plan visibility

--- one-click-infra/iam.tf
+++ one-click-infra/iam.tf
@@ -359,3 +359,7 @@ resource "aws_iam_role_policy_attachment" "jenkins_tfstate_attach" {
   role       = aws_iam_role.jenkins_irsa.name
   policy_arn = aws_iam_policy.jenkins_tfstate.arn
 }
+resource "aws_iam_role_policy_attachment" "jenkins_readonly" {
+  role       = aws_iam_role.jenkins_irsa.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
